ff494d5aec6845a850fa5562b6d48fca046953b0
Tamilarasu Ubuntu <tamilarasu@ubuntu.com>
2022-12-18 15:08:07 +0530

    test

--- components/ClientReload.js
+++ components/ClientReload.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import Router from 'next/router'
 
 /**
- * Client-side complement to next-remote-watch
+ * Client-side test complement to next-remote-watch
  * Re-triggers getStaticProps when watched mdx files change
  *
  */
